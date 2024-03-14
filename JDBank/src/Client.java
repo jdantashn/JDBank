b156ca3eeead153797c8282328ae7dbf8a885f8d
jdantashn <161704908+jdantashn@users.noreply.github.com>
2024-03-14 15:15:00 +0000

    1.1 Client commit

---
+++ JDBank/src/Client.java
@@ -0,0 +1,38 @@
+
+public class Client {
+
+	private static int nextId = 0;
+	private int clientNumber;
+	private String name;
+	private String firstName;
+	
+	  // Constructor
+	  public Client(String name, String firstName) {
+	    clientNumber = ++nextId;
+	    this.name = name;
+	    this.firstName = firstName;
+	  }
+	  
+		public String getName() {
+			return name;
+		}
+		
+		public String getFirstName() {
+			return firstName;
+		}
+		
+		public int getClientNumber() {
+			return clientNumber;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		
+
+
+}
